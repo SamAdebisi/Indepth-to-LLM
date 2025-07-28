6e7e837dc03e0b5d83c9ba12dc3f97a5e56cf906
Azeez Adebisi <samadebisi12@gmail.com>
2025-07-29 02:15:17 +0300

    Update make_transformer.py

--- make_transformer.py
+++ make_transformer.py
@@ -2,3 +2,17 @@
 uses super state of the art Transformer AI tech
 this code is intended to be super hackable.""" 
 
+import os 
+import sys 
+import time 
+import math 
+import argparse 
+from dataclasses import dataclass 
+from typing import List 
+
+import torch 
+import torch.nn as nn 
+from torch.nn import functional as F 
+from torch.utils.data import Dataset 
+from torch.utils.data.dataloader import DataLoader 
+from torch.utils.tensorboard import SummaryWriter 
