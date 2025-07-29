3bb578ddc030b3d7412dac3ec6d6a04d5ba2411a
Azeez Adebisi <samadebisi12@gmail.com>
2025-07-29 06:22:32 +0300

    Update make_transformer.py

--- make_transformer.py
+++ make_transformer.py
@@ -221,3 +221,27 @@ class BoW(nn.Module):
         self.lm_head = nn.Linear(config.n_embd, self.vocab_size)
 
         return self.block_size 
+    
+    def forward(self, idx, targets=None):
+
+        device = idx.device
+        b, t = idx.size()
+        assert t <= self.block_size, f"Cannot forward sequence of length {t}, block size is only {self.block_size}"
+        pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(0) # shape (1, t)
+
+        # forward the token and position embedding layers
+        tok_emb = self.wte(idx) # token embeddings of shape (b, t, n_embd)
+        pos_emb = self.wpe(pos) # position embeddings of shape (1, t, n_embd)
+        # add and run through the decoder MLP
+        x = tok_emb + pos_emb
+        # run the bag of words context module
+        x = self.context_block(x)
+        # decode to next token probability
+        logits = self.lm_head(x)
+
+        # if we are given some desired targets also calculate the loss
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+
+        return logits, loss
